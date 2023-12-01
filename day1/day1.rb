46d3441931a7cd5a5a3d0534e125ad09df97d64c
Ryan Bibby <ryan.bibby@bjss.com>
2023-12-01 22:16:32 +0000

    Day 1: Tidy up

--- day1/day1.rb
+++ day1/day1.rb
@@ -2,20 +2,12 @@
 puts File.open("input.txt").map { |line| "#{line.scan(/\d/).first}#{line.scan(/\d/).last}".to_i }.sum
 
 # Part 2
-def subWordsForNumbers(line) 
-  line =  line.sub("one", "1")
-  line =  line.sub("two", "2")
-  line =  line.sub("three", "3")
-  line =  line.sub("four", "4")
-  line =  line.sub("five", "5")
-  line =  line.sub("six", "6")
-  line =  line.sub("seven", "7")
-  line =  line.sub("eight", "8")
-  line =  line.sub("nine", "9")
+def sub_words_for_numbers(line)
+  line.sub(/\D+/, "one" => "1", "two" => "2", "three" => "3", "four" => "4", "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine"=> "9")
 end
 
 puts(File.open("input.txt").map do |line|
-  first = subWordsForNumbers(line.scan(/\d|one|two|three|four|five|six|seven|eight|nine/).first)
-  last = subWordsForNumbers(line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten.last)
+  first = sub_words_for_numbers(line.scan(/\d|one|two|three|four|five|six|seven|eight|nine/).first)
+  last = sub_words_for_numbers(line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten.last)
   "#{first}#{last}".to_i
 end.sum)
