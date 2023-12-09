1ea060f183eb9a82e08cf7b655d784dc635dd7aa
Ryan Bibby <ryan.bibby@bjss.com>
2023-12-09 14:50:17 +0000

    Day 7

---
+++ day7/day7.rb
@@ -0,0 +1,18 @@
+def get_rank(raw_rank)
+  { "A" => 14, "K" => 13, "Q" => 12, "J" => 11 , "T" => 10 }[raw_rank] || raw_rank.to_i
+end
+
+hands = File.open("input.txt").map do |line|
+  cards_string, bid = line.split(" ")
+  { cards: cards_string.split(""), bid: bid.to_i }
+end
+
+sorted_hands = hands.map do |hand|
+  values = hand[:cards].map { |card| get_rank(card) }
+  totals = values.each_with_object(Hash.new(0)) { |value, totals| totals[value] += 1 }.sort_by { |k,v| -v }.to_h
+  { totals: totals, values: values, bid: hand[:bid] }
+end.sort_by { |hash| [hash[:totals].values, hash[:values]]  }
+
+pp(sorted_hands.map.with_index do |hand, i|
+  hand[:bid] * (i + 1)
+end.sum)
