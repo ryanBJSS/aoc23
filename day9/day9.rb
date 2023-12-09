15754b0bc58b3e9168898e4f16d691358b20992e
Ryan Bibby <ryan.bibby@bjss.com>
2023-12-09 18:17:05 +0000

    Day 9: tidy

--- day9/day9.rb
+++ day9/day9.rb
@@ -13,11 +13,7 @@ def calculate(lines)
 
     processed_lines = processed_lines.reverse
     processed_lines.each_with_index do |l, i|
-      if i == 0
-        processed_lines[i] << 0
-      else
-        processed_lines[i] << processed_lines[i-1][-1] + l[-1]
-      end
+      l << (i == 0 ? 0 : processed_lines[i-1][-1] + l[-1])
     end
     processed_lines[-1][-1]
   end
