7b31e63303228d806d2346d514a338330903ed46
Ryan Bibby <ryan.bibby@bjss.com>
2023-12-09 18:11:30 +0000

    Day 9: tidy

--- day9/day9.rb
+++ day9/day9.rb
@@ -1,36 +1,26 @@
-def process_line(line)
-  line.each_cons(2).map do |a,b|
-    b - a
-  end
-end
-
-lines = File.open("input.txt").map { |l| l.split(" ") }
-
+lines = File.open("input.txt").map { |l| l.split(" ").map(&:to_i) }
 def calculate(lines)
-  predictions = []
-  lines.each do |line|
 
-    processed_lines = []
-    line = line.map(&:to_i)
+  lines.map do |line|
 
-    processed_lines << line
+    processed_lines = [line]
+
     while line.reject { |n| n == 0 }.size != 0
-      next_line = process_line(line)
+      next_line = line.each_cons(2).map { |a,b| b - a }
       processed_lines << next_line
       line = next_line
     end
 
     processed_lines = processed_lines.reverse
-    processed_lines.each_with_index do |line, i|
+    processed_lines.each_with_index do |l, i|
       if i == 0
         processed_lines[i] << 0
       else
-        processed_lines[i] << processed_lines[i-1][-1] + line[-1]
+        processed_lines[i] << processed_lines[i-1][-1] + l[-1]
       end
     end
-    predictions << processed_lines[-1][-1]
+    processed_lines[-1][-1]
   end
-  predictions
 end
 
 pp calculate(lines).sum
