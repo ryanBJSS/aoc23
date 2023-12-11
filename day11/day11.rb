ab2c630037cb46626d2010bdd876a2f385bc28cb
Ryan Bibby <ryan.bibby@bjss.com>
2023-12-11 12:11:32 +0000

    Day 11: Slight tidy

--- day11/day11.rb
+++ day11/day11.rb
@@ -18,8 +18,6 @@ class NodeMap
   def get_with_j(j)
     @nodes.select { |node| node.j == j }
   end
-
-
 end
 
 class Node
@@ -29,55 +27,51 @@ class Node
     @j = j
   end
 
-  def expand_in_i
-    @i += 1
+  def expand_in_i(n)
+    @i += n
   end
 
-  def expand_in_j
-    @j += 1
+  def expand_in_j(n)
+    @j += n
   end
 end
 
+@node_map = NodeMap.new
 
-node_map = NodeMap.new
-
-empty_rows = []
-empty_cols = []
-row_lenth = nil
 File.open("input.txt").map(&:chomp).each_with_index do |line, i|
-  row_lenth = line.size
-  empty_rows << i if line.chars.reject { |c| c == "." }.size == 0
+  @grid_length = line.size
   line.chars.each_with_index do |row, j|
-    node_map.add Node.new(i, j) if row != "."
+    @node_map.add Node.new(i, j) if row != "."
   end
 end
-# pp node_map
-pp empty_rows
 
-row_lenth.times do |j|
-  empty_cols << j if node_map.get_with_j(j).empty?
+@empty_rows = []
+@grid_length.times do |i|
+  @empty_rows << i if @node_map.get_with_i(i).empty?
 end
 
-
-empty_rows = empty_rows.map.with_index { |v, i| v + i }
-empty_cols = empty_cols.map.with_index { |v, i| v + i }
-
-pp empty_rows
-
-empty_rows.each do |row|
-  node_map.nodes.select { |node| node.i > row }.map(&:expand_in_i)
+@empty_cols = []
+@grid_length.times do |j|
+  @empty_cols << j if @node_map.get_with_j(j).empty?
 end
 
-empty_cols.each do |col|
-  node_map.nodes.select { |node| node.j > col }.map(&:expand_in_j)
+def total_distances (expansion_size)
+  node_map = Marshal.load(Marshal.dump(@node_map))
+  empty_rows = @empty_rows.map.with_index { |v, i| v + expansion_size * i }
+  empty_cols = @empty_cols.map.with_index { |v, i| v + expansion_size * i }
+
+  empty_rows.each do |row|
+    node_map.nodes.select { |node| node.i > row }.map { |node| node.expand_in_i(expansion_size) }
+  end
+
+  empty_cols.each do |col|
+    node_map.nodes.select { |node| node.j > col }.map { |node| node.expand_in_j(expansion_size) }
+  end
+
+  pp(node_map.nodes.combination(2).map do |node1, node2|
+    (node1.i - node2.i).abs + (node1.j - node2.j).abs
+  end.sum)
 end
 
-pp node_map.nodes
-
-
-distances = []
-node_map.nodes.combination(2) do |node1, node2|
-  distances << ((node1.i) - node2.i).abs + (node1.j - node2.j).abs
-end
-
-pp distances.sum
+total_distances(1)
+total_distances(999999)
